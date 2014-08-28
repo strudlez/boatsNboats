243bd19248de3672c5f3420c87f72f365f7298f4
Ben Weitzman <benjamin.weitzman@tufts.edu>
2014-08-27 21:48:16 -0400

    delete garbage from fluid solver

--- core/src/com/boats/n/containers/Grids.java
+++ core/src/com/boats/n/containers/Grids.java
@@ -2,7 +2,6 @@ package com.boats.n.containers;
 
 import com.boats.n.functions.Isomorphism;
 import com.google.common.base.Function;
-import com.google.common.collect.ImmutableTable;
 import com.google.common.collect.Iterators;
 import com.google.common.collect.Table;
 
@@ -59,112 +58,6 @@ public class Grids
                 return transformCellIterable(from.iterator(), iso.forwardFunction());
             }
         };
-    }
-
-    public static <T1, T2> Pair<? extends Grid<T1>, ? extends Grid<T2>> split(final Grid<Pair<T1, T2>> grid)
-    {
-        return Pair.of(new Grid<T1>()
-                       {
-                           @Override
-                           public Iterable<Integer> getYs()
-                           {
-                               return grid.getYs();
-                           }
-
-                           @Override
-                           public Iterable<Integer> getXs()
-                           {
-                               return grid.getXs();
-                           }
-
-                           @Override
-                           public T1 get(int x, int y)
-                           {
-                               return grid.get(x, y).getFirst();
-                           }
-
-                           @Override
-                           public void put(int x, int y, T1 value)
-                           {
-                               grid.put(x, y, Pair.of(value, grid.get(x, y).getSecond()));
-                           }
-
-                           @Override
-                           public int getWidth()
-                           {
-                               return grid.getWidth();
-                           }
-
-                           @Override
-                           public int getHeight()
-                           {
-                               return grid.getHeight();
-                           }
-
-                           @Override
-                           public Iterator<Table.Cell<Integer, Integer, T1>> iterator()
-                           {
-                               return transformCellIterable(grid.iterator(), new Function<Pair<T1, T2>, T1>()
-                               {
-                                   @Override
-                                   public T1 apply(Pair<T1, T2> input)
-                                   {
-                                       return input.getFirst();
-                                   }
-                               });
-                           }
-                       }, new Grid<T2>()
-                       {
-                           @Override
-                           public Iterable<Integer> getYs()
-                           {
-                               return grid.getYs();
-                           }
-
-                           @Override
-                           public Iterable<Integer> getXs()
-                           {
-                               return grid.getXs();
-                           }
-
-                           @Override
-                           public T2 get(int x, int y)
-                           {
-                               return grid.get(x, y).getSecond();
-                           }
-
-                           @Override
-                           public void put(int x, int y, T2 value)
-                           {
-                               grid.put(x, y, Pair.of(grid.get(x, y).getFirst(), value));
-                           }
-
-                           @Override
-                           public int getWidth()
-                           {
-                               return grid.getWidth();
-                           }
-
-                           @Override
-                           public int getHeight()
-                           {
-                               return grid.getHeight();
-                           }
-
-                           @Override
-                           public Iterator<Table.Cell<Integer, Integer, T2>> iterator()
-                           {
-                               return transformCellIterable(grid.iterator(), new Function<Pair<T1, T2>, T2>()
-                               {
-                                   @Override
-                                   public T2 apply(Pair<T1, T2> input)
-                                   {
-                                       return input.getSecond();
-                                   }
-                               });
-                           }
-                       }
-        );
     }
 
     private static <F, T> Iterator<Table.Cell<Integer, Integer, T>> transformCellIterable(Iterator<Table.Cell<Integer, Integer, F>> iterator, final Function<? super F, ? extends T> function)
