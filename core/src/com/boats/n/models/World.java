3941b9428be4358badf6af92809f0703668486ef
Zach Strudler <strudlez@gmail.com>
2014-09-21 17:08:51 -0400

    java 6

--- core/src/com/boats/n/models/World.java
+++ core/src/com/boats/n/models/World.java
@@ -1,11 +1,11 @@
 package com.boats.n.models;
 
-import java.util.ArrayList;
 import java.util.Collection;
 
 import com.badlogic.gdx.math.Vector2;
 import com.boats.n.models.n.boats.TinyBoat;
 import com.boats.n.physics.PhysicsWorld;
+import com.google.common.collect.Lists;
 
 /**
  * Created by ben on 8/26/14.
@@ -24,7 +24,7 @@ public class World
         this.height = height;
         ocean = new Ocean(width, height);
         physicsWorld = new PhysicsWorld();
-        boats = new ArrayList<>();
+        boats = Lists.newArrayList();
 
         boats.add(new TinyBoat(physicsWorld, new Vector2(50, 50)));
     }
